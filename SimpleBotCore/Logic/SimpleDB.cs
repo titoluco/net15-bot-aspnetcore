7d46c4e7e1bbc3a5e0c42b7685c3fab83def1127
Logon Aluno <logonrmlocal@fiap.com.br>
2019-06-08 10:29:36 -0300

    final

--- SimpleBotCore/Logic/SimpleDB.cs
+++ SimpleBotCore/Logic/SimpleDB.cs
@@ -11,12 +11,12 @@ namespace SimpleBotCore.Logic
     {
         public static IMongoClient client { get; set; }
         public static IMongoDatabase db { get; set; }
-        public static IMongoCollection<SimpleMessage> col { get; set; }
+        public static IMongoCollection<BsonDocument> col { get; set; }
         public static void Iniciar()
         {
             client = new MongoClient("mongodb://localhost:27017");
             db = client.GetDatabase("15NET");
-            col = db.GetCollection<SimpleMessage>("novo");
+            col = db.GetCollection<BsonDocument>("novo");
         }
         public static void createDoc()
         {
@@ -33,16 +33,18 @@ namespace SimpleBotCore.Logic
             //    qtdMensagens = qtdMsg
             //};
 
-            /*
+
             var doc = new BsonDocument() {
-                { "campo1", 1 },
-                { "campo2", 2 }};
-            */
-            col.InsertOne(message);
+                { "Id", message.Id },
+                { "User", message.User },
+                { "Text", message.Text }
+            };
+
+            col.InsertOne(doc);
         }
         public static int getDoc(string Id)
         {
-            var filter = Builders<SimpleMessage>.Filter.Eq("Id", Id);
+            var filter = Builders<BsonDocument>.Filter.Eq("Id", Id);
             var results = SimpleDB.col.Find(filter).ToList();
             return results.Count();
         }
